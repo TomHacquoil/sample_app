dafa95eedb8f6139ce360f38cca83adc66559d7f
Tom Hacquoil <thacquoil@infuse-ipm.com>
2010-10-17 23:01:00 +0000

    Added help page, optimised test suite

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe PagesController do
 	render_views
 
+	before(:each) do
+	
+		@base_title = "Ruby on Rails Sample Application"
+
+	end
+
+
   describe "GET 'home'" do
 
     it "should be successful" do
@@ -12,9 +19,8 @@ describe PagesController do
 
 		it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Sample Application | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
-
 
   end
 
@@ -28,7 +34,7 @@ describe PagesController do
 		
 		it "should have the right title" do
 			get 'contact'
-			response.should have_selector("title", :content => "Ruby on Rails Sample Application | Contact")
+			response.should have_selector("title", :content => @base_title + " | Contact")
 		end
 
   end
@@ -43,8 +49,22 @@ describe PagesController do
 
 		it "should have the right title" do
 			get 'about'
-			response.should have_selector("title", :content => "Ruby on Rails Sample Application | About")
+			response.should have_selector("title", :content => @base_title + " | About")
 		end
+
+  end
+
+	describe "GET 'help'" do
+
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+		it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
+    end
 
   end
 

b1aa3518751984485a867393ceb910932d1ce753
Tom Hacquoil <thacquoil@infuse-ipm.com>
2010-10-18 17:08:39 +0100

    Created Database, written tests

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+	attr_accessible :name, :email
+
+	validates :name, :presence => true
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe User do
+
+  before(:each) do
+		@attr = { :name => "Example User", :email => "user@example.com" }
+	end
+
+	it "should create a new instance given valid attributes" do
+		User.create!(@attr)
+	end
+
+	it "should require a name"
+		no_name_user = User.new(@attr.merge(:name => ""))
+		no_name_user.should_not be_valid
+	end
+
+end
